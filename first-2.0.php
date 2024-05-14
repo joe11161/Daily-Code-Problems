130a83fb9a680a375b149d4eb662e474ad29627c
Duncan I. Mwangi <irungu.mwangi2@gmail.com>
2024-05-14 09:10:13 +0300

    Add testing and more optimal solution

--- first-2.0.php
+++ first-2.0.php
@@ -1,7 +1,7 @@
 <?php
-	var_dump(result([10, 4, 3, 7], 17));
+	var_dump(solutionB([10, 4, 3, 7], 17));
 
-	function result($nums, $k)
+	function solutionB($nums, $k)
 	{
 		foreach ($nums as $i => $num) {
 			foreach ($nums as $j =>  $value) {
@@ -17,5 +17,39 @@
 		}
 		return false;
 	}
-?>
- 
+
+
+function solutionA($nums, $k) {
+	$count = count($nums);
+	foreach ($nums as $i => $num) {
+		for($j = $i+1; $j < $count; $j++) {
+			$total = $num + $nums[$j];
+			if ($k == $total) {
+				return true;
+			}
+		}
+		if($i+2 >= $count) {
+			break;
+		}
+	}
+	return false;
+}
+
+
+$nums = [10, 4, 3, 7];
+$k = 17;
+$nums = range(0, 10000);
+$k = $nums[count($nums)-1] + $nums[count($nums)-2];
+
+$time_start = microtime(true);
+$result = solutionA($nums, $k);
+$time_end = microtime(true);
+$timeUsed = $time_end - $time_start;
+echo "\n".'solutionA '.$result .' Time: '. number_format($timeUsed,8) . "\n";
+
+
+$time_start = microtime(true);
+$result = solutionB($nums, $k);
+$time_end = microtime(true);
+$timeUsed = $time_end - $time_start;
+echo 'solutionB '.$result .' Time: '. number_format($timeUsed,8) . "\n";
