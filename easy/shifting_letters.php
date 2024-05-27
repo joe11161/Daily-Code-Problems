5b9e6d5b4352f5857d776f8204a259ced32c6334
Joe Mwangi <152086705+joe11161@users.noreply.github.com>
2024-05-27 15:04:16 +0300

    Create shifting_letters.php
    Shifting letters while comparing to a different string

---
+++ easy/shifting_letters.php
@@ -0,0 +1,67 @@
+<!-- This problem was asked by Google.
+
+Given two strings A and B, return whether or not A can be shifted some number of times to get B.
+
+For example, if A is abcde and B is cdeab, return true. If A is abc and B is acb, return false. -->
+
+
+
+<?php
+//solution A
+ function stringtoArray($delimiter, $string,$compare) {
+ 	$array =explode($delimiter, $string);
+    $len = count($array);
+    switch (true){
+    	case ($len === 0):
+        	echo "Please add a string";
+            break;
+        case ($len >= 2):              
+         if($len === 2){
+         	$val1 = [$array[1], $array[0]];
+            $val2 = implode($val1);         
+           $test = ($val2==$compare)? true :false;
+           
+           return $test;
+         }
+    }
+ }
+
+
+//solution B
+
+class stringToArray{
+    public $string;
+    public $delimiter;
+    public $compare;
+    public function __construct($string, $delimiter, $compare){
+        $this->string = $string;
+        $this->delimiter = $delimiter;
+        $this->compare = $compare;
+
+        $array =explode($delimiter, $string);
+    $len = count($array);
+    switch (true){
+    	case ($len === 0):
+        	echo "Please add a string";
+            break;
+        case ($len >= 2):              
+         if($len === 2){
+         	$val1 = [$array[1], $array[0]];
+            $val2 = implode($val1);         
+           $test = ($val2==$compare)? true :false;
+        var_dump($test);           
+         }
+       
+    }
+    }
+}
+
+$string1= "ab,cde";
+$string2= "a,bc";
+$delimiter = ",";
+$compare1= "cdeab";
+$compare2= "bca";
+
+new stringToArray($string1, $delimiter, $compare1);
+var_dump(stringtoArray($delimiter, $string1, $compare1));
+?>
